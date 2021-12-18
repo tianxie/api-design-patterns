2877188d8c346287359ce743ffc42acc79fddf92
tianxie <txie@live.cn>
2021-12-18 22:15:18 +0800

    初始化项目

---
+++ src/main/java/com/github/tianxie/apidesignpatterns/ApiDesignPatternsApplication.java
@@ -0,0 +1,13 @@
+package com.github.tianxie.apidesignpatterns;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiDesignPatternsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiDesignPatternsApplication.class, args);
+    }
+
+}
